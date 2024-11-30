74ee1d91c4151b933815b56133e6056f5a353807
Tan Kiet <22130132@st.hcmuaf.edu.vn>
2024-11-30 17:49:21 +0700

    chinh sua trang them san pham

--- admin/assets/js/updatePreviewImage.js
+++ admin/assets/js/updatePreviewImage.js
@@ -1,10 +1,8 @@
-function updatePreviewImage(input, index) {
-    const file = input.files[0]; // Lấy tệp đầu tiên từ input
-    const preview = input.closest('.upload-image').querySelector('.preview');
-    const addIcon = input.closest('.upload-image').querySelector('.add-img-icon');
-    addIcon.classList.add('d-none');
-    // preview.src = e.target.result; // Gán nội dung file vào thuộc tính `src`
-    preview.src = URL.createObjectURL(file);
-    // active next upload image
-    document.querySelector(`.upload-image.disabled`).classList.remove('disabled');
-}
+$(document).ready(function () {
+   $(document).on('change', '.upload-image input[type="file"]', function (){
+       const file = this.files[0];
+       $(this).closest('.upload-image').find('.preview').attr('src', URL.createObjectURL(file));0
+       $(this).closest('.upload-image').find('.add-img-icon').remove();
+       console.log(URL.createObjectURL(file));
+   });
+});
