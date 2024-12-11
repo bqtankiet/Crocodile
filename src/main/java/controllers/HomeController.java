8ae9f2606010e4ab94382a001257bd51a13e558d
Tan Kiet <22130132@st.hcmuaf.edu.vn>
2024-12-11 22:09:14 +0700

    fix bug

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -6,7 +6,7 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
-@WebServlet(name = "HomeController", value = "/")
+@WebServlet(name = "HomeController", value = "")
 public class HomeController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
