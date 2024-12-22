43915fb5b026b77787a7de359a410c52b883b1bc
ahkiet123 <22130133@st.hcmuaf.edu.vn>
2024-12-22 21:59:34 +0700

    build Google login Controller

---
+++ src/main/java/vn/edu/hcmuaf/fit/crocodile/controller/GoogleLoginController.java
@@ -0,0 +1,49 @@
+package vn.edu.hcmuaf.fit.crocodile.controller;
+
+import vn.edu.hcmuaf.fit.crocodile.constant.Iconstant;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+import org.json.JSONObject;
+
+public class GoogleLoginController {
+    public static String getAccessToken(String authorizationCode) throws IOException {
+        // 1. Tạo URL kết nối
+        URL url = new URL(Iconstant.GOOGLE_LINK_GET_TOKEN);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        // 2. Tạo nội dung yêu cầu
+        String params = "code=" + authorizationCode
+                + "&client_id=" + Iconstant.GOOGLE_CLIENT_ID
+                + "&client_secret=" + Iconstant.GOOGLE_CLIENT_SECRET
+                + "&redirect_uri=" + Iconstant.GOOGLE_REDIRECT_URI
+                + "&grant_type=" + Iconstant.GOOGLE_GRANT_TYPE;
+        // 3. Gửi yêu cầu
+        try (OutputStream os = connection.getOutputStream()) {
+            os.write(params.getBytes());
+            os.flush();
+        }
+        // 4. Kiểm tra phản hồi từ Google
+        int responseCode = connection.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            // 5. Đọc phản hồi
+            try (Scanner scanner = new Scanner(connection.getInputStream())) {
+                StringBuilder response = new StringBuilder();
+                while (scanner.hasNextLine()) {
+                    response.append(scanner.nextLine());
+                }
+                // 6. Giải mã JSON và lấy access token
+                JSONObject jsonResponse = new JSONObject(response.toString());
+                return jsonResponse.getString("access_token");
+            }
+        } else {
+            throw new IOException("Error while getting access token. HTTP Code: " + responseCode);
+        }
+    }
+}
--- src/main/java/vn/edu/hcmuaf/fit/crocodile/controller/SignupController.java
+++ src/main/java/vn/edu/hcmuaf/fit/crocodile/controller/SignupController.java
@@ -6,14 +6,33 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
-@WebServlet(name = "SignupController", value = "/signup")
+@WebServlet(name = "SignupController", urlPatterns = {"/signup"})
 public class SignupController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/views/signup.jsp").forward(request, response);
+        processRequest(request, response);
+
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        processRequest(request, response);
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String code = request.getParameter("code");
+        try {
+            GoogleLoginController googleLogin = new GoogleLoginController();
+            String accessToken = GoogleLoginController.getAccessToken(code);
+            System.out.println("Access token: " + accessToken);
+            // Chuyển hướng về home sau khi hoàn tất
+            response.sendRedirect("http://localhost:8080/crocodile/home");
+        } catch (IOException e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Unable to process Google login.");
+        }
     }
 }
