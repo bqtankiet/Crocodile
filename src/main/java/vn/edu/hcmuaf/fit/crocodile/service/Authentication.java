10a97147edee3d9c52fb8e329344b5162624eebd
ahkiet123 <22130133@st.hcmuaf.edu.vn>
2024-12-26 16:44:21 +0700

    add hash MD5 password and test login + signup

---
+++ src/main/java/vn/edu/hcmuaf/fit/crocodile/service/Authentication.java
@@ -0,0 +1,44 @@
+package vn.edu.hcmuaf.fit.crocodile.service;
+
+import vn.edu.hcmuaf.fit.crocodile.dao.user.UserDao;
+import vn.edu.hcmuaf.fit.crocodile.dao.user.UserDaoImpl;
+import vn.edu.hcmuaf.fit.crocodile.model.entity.User;
+import vn.edu.hcmuaf.fit.crocodile.util.HashUtil;
+
+import java.util.Optional;
+
+public class Authentication {
+    private final UserDao userDao;
+
+    public Authentication() {
+        this.userDao = new UserDaoImpl();
+    }
+
+    /**
+     * Xác thực dựa trên username và password.
+     *
+     * @return ID của user nếu thành công, -1 nếu thất bại.
+     */
+    public int login(String username, String password) {
+        Optional<User> optionalUser = userDao.findByUsername(username);
+
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            String hashedPassword = HashUtil.hashMD5(password);
+            System.out.println("mật khẩu đã hash   " + hashedPassword);
+            System.out.println("mật khẩu lưu ở DB: " + user.getPassword());
+            if (hashedPassword.equals(user.getPassword())) {
+                return user.getId();
+            }
+        }
+
+        return -1;
+    }
+
+    public int signup(User user) {
+        user.setPassword(HashUtil.hashMD5(user.getPassword()));
+        return userDao.create(user);
+    }
+
+
+}
---
+++ src/test/java/AuthenticationTest.java
@@ -0,0 +1,41 @@
+import vn.edu.hcmuaf.fit.crocodile.model.entity.User;
+import vn.edu.hcmuaf.fit.crocodile.service.Authentication;
+
+public class AuthenticationTest {
+    public static void main(String[] args) {
+        Authentication auth = new Authentication();
+
+        System.out.println("===== Test =====");
+//        // Test với người dùng đã tồn tại
+//        String username = "admin";
+//        String correctPassword = "Admin@123";  // Mật khẩu đúng
+//        String wrongPassword = "incorrectPassword"; // Mật khẩu sai
+//
+//        // Test login với mật khẩu đúng
+//        int userId = auth.login(username, correctPassword);
+//        System.out.println("Login với mật khẩu đúng: " + (userId != -1 ? "Thành công (User ID: " + userId + ")" : "Thất bại"));
+//
+//        // Test login với mật khẩu sai
+//        int userId1 = auth.login(username, wrongPassword);
+//        System.out.println("Login với mật khẩu sai: " + (userId1 != -1 ? "Thành công (User ID: " + userId1 + ")" : "Thất bại"));
+//
+        String unameTest = "dmk";
+        String passTest = "dmk";
+
+        User newUser = new User();
+
+        newUser.setUsername(unameTest);
+        newUser.setPassword(passTest);
+
+        int newUserId = auth.signup(newUser); // Đăng ký người dùng mới
+        System.out.println("Đăng ký tk dmk:  " + (newUserId != -1 ? "Thành công (User ID: " + newUserId + ")" : "Thất bại"));
+
+        newUserId = auth.login(unameTest, passTest);
+        System.out.println("Login tk dmk: " + (newUserId != -1 ? "Thành công (User ID: " + newUserId + ")" : "Thất bại"));
+
+//        int dak = auth.login("dak", "dak");
+//        // Kiểm tra đăng nhập lại với thông tin người dùng mới
+////        int newUserLoginId = auth.login(username1, password1);
+//        System.out.println("Login với người dùng mới: " + (dak != -1 ? "Thành công (User ID: " + dak + ")" : "Thất bại"));
+    }
+}
