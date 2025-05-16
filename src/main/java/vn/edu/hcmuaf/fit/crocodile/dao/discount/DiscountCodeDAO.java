6a3fe7ef562680b4db860021c431a6e9e99a4a7c
Tan Kiet <bqtankiet@gmail.com>
2025-05-17 00:01:30 +0700

    add: DiscountCodeDAO

---
+++ src/main/java/vn/edu/hcmuaf/fit/crocodile/dao/discount/DiscountCodeDAO.java
@@ -0,0 +1,70 @@
+package vn.edu.hcmuaf.fit.crocodile.dao.discount;
+
+import vn.edu.hcmuaf.fit.crocodile.config.JdbiConnect;
+import vn.edu.hcmuaf.fit.crocodile.model.entity.DiscountCode;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class DiscountCodeDAO implements IDiscountCodeDAO {
+    @Override
+    public void createDiscountCode(String code, DiscountCode.DiscountType type, BigDecimal value, BigDecimal maxDiscount, LocalDateTime startDate, LocalDateTime endDate, int maxUses, BigDecimal minOrderValue) {
+        JdbiConnect.getJdbi().withHandle(handle ->
+                handle.createUpdate("INSERT INTO " + DiscountCode.TABLE + " (code, type, value, maxDiscount, startDate, endDate, maxUses, minOrderValue, status) VALUES (:code, :type, :value, :maxDiscount, :startDate, :endDate, :maxUses, :minOrderValue, 'ACTIVE')")
+                        .bind("code", code)
+                        .bind("type", type)
+                        .bind("value", value)
+                        .bind("maxDiscount", maxDiscount)
+                        .bind("startDate", startDate)
+                        .bind("endDate", endDate)
+                        .bind("maxUses", maxUses)
+                        .bind("minOrderValue", minOrderValue)
+                        .execute()
+        );
+    }
+
+    @Override
+    public void createDiscountCode(DiscountCode discountCode) {
+        createDiscountCode(
+                discountCode.getCode(),
+                discountCode.getType(),
+                discountCode.getValue(),
+                discountCode.getMaxDiscount(),
+                discountCode.getStartDate(),
+                discountCode.getEndDate(),
+                discountCode.getMaxUses(),
+                discountCode.getMinOrderValue()
+        );
+    }
+
+    @Override
+    public DiscountCode findByCode(String code) {
+        return JdbiConnect.getJdbi().withHandle(handle ->
+                handle.createQuery("SELECT * FROM "+DiscountCode.TABLE+" WHERE code = :code")
+                        .bind("code", code)
+                        .mapToBean(DiscountCode.class)
+                        .findFirst()
+                        .orElse(null));
+    }
+
+    @Override
+    public DiscountCode findById(int id) {
+        return JdbiConnect.getJdbi().withHandle(handle ->
+                handle.createQuery("SELECT * FROM "+DiscountCode.TABLE+" WHERE id = :id")
+                        .bind("id", id)
+                        .mapToBean(DiscountCode.class)
+                        .findFirst()
+                        .orElse(null));
+    }
+
+    @Override
+    public List<DiscountCode> findAll() {
+        return JdbiConnect.getJdbi().withHandle(handle ->
+                handle.createQuery("SELECT * FROM " + DiscountCode.TABLE)
+                        .mapToBean(DiscountCode.class)
+                        .list()
+        );
+    }
+
+}
---
+++ src/main/java/vn/edu/hcmuaf/fit/crocodile/dao/discount/IDiscountCodeDAO.java
@@ -0,0 +1,20 @@
+package vn.edu.hcmuaf.fit.crocodile.dao.discount;
+
+import vn.edu.hcmuaf.fit.crocodile.model.entity.DiscountCode;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface IDiscountCodeDAO {
+
+    void createDiscountCode(String code, DiscountCode.DiscountType type, BigDecimal value, BigDecimal maxDiscount, LocalDateTime startDate, LocalDateTime endDate, int maxUses, BigDecimal minOrderValue);
+
+    void createDiscountCode(DiscountCode discountCode);
+
+    DiscountCode findByCode(String code);
+
+    DiscountCode findById(int id);
+
+    List<DiscountCode> findAll();
+}
